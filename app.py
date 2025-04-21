948dde2b5ebc18db93ea07227ce8b471e8e89320
sree1975-cyber <sreeduba@hotmail.com>
2025-04-21 19:16:42 -0400

    Update app.py

--- app.py
+++ app.py
@@ -557,8 +557,8 @@ def main():
         # Individual student prediction section
         # render_individual_prediction()----COMMINTING THIS DUE TO NO NEED TO USE HERE
         plot_risk_gauge()
-        get_risk_explanation()
-       # get_recommendation_with_reasons()
+        #get_risk_explanation()
+        #get_recommendation_with_reasons()
             
     elif app_mode == "Advanced Analytics":
         render_advanced_analytics()
